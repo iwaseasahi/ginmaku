7db321db3f33fa47b68da6e875357fff4304329a
岩瀬旭 <eb50113@yahoo.co.jp>
2018-03-17 12:58:11 +0900

    BASIC認証を戻す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # before_filter :authenticate
-  #
-  # private
-  #
-  # def authenticate
-  #   # BASIC認証
-  #   authenticate_or_request_with_http_basic('Administration') do |name, password|
-  #     name == ENV['BASIC_USERNAME'] && password == ENV['BASIC_PASSWORD']
-  #   end
-  # end
+  before_filter :authenticate
+
+  private
+
+  def authenticate
+    # BASIC認証
+    authenticate_or_request_with_http_basic('Administration') do |name, password|
+      name == ENV['BASIC_USERNAME'] && password == ENV['BASIC_PASSWORD']
+    end
+  end
 end
